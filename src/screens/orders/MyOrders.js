37e3a322f40851515cdea1cb9cecaad909344bb7
Shamroz <shamroz.rajpoot1@gmail.com>
2019-09-04 22:00:47 +0500

    new home changes

--- src/screens/orders/MyOrders.js
+++ src/screens/orders/MyOrders.js
@@ -1,7 +1,21 @@
-import React,{useEffect} from 'react'
+import React,{useState,useEffect} from 'react'
 import {View,Text,StatusBar,Platform,StyleSheet} from 'react-native'
-import { Container, Header, Left, Body, Right, Button, Icon, Title,Content,H1,Tab,Tabs,Card, CardItem } from 'native-base';
-export default function MyOrders(props) {
+import { Container, Header, Left, Body, Right, Button, Icon, Title,Content,H2,Tab,Tabs,Card, CardItem,Item,Input } from 'native-base';
+function MyOrders(props) {
+
+      const [state,setState] = useState({
+        searchBar:false,
+        orders: [
+            { orderTitle: 'Malik Corporation', orderDetail:'Order #: PK0000517 | Order Date: 2019-08-24 | Delivery Date: 2019-08-28',id:1 },
+            { orderTitle: 'Sahara Corporation', orderDetail:'Order #: PK0000517 | Order Date: 2019-08-24 | Delivery Date: 2019-08-28',id:2 },
+            { orderTitle: 'Sitara Corporation', orderDetail:'Order #: PK0000517 | Order Date: 2019-08-24 | Delivery Date: 2019-08-28',id:3 },
+            { orderTitle: 'Dina Corporation', orderDetail:'Order #: PK0000517 | Order Date: 2019-08-24 | Delivery Date: 2019-08-28',id:4 },
+            { orderTitle: 'Lahore Corporation', orderDetail:'Order #: PK0000517 | Order Date: 2019-08-24 | Delivery Date: 2019-08-28',id:5 },
+            { orderTitle: 'Ziarat Corporation', orderDetail:'Order #: PK0000517 | Order Date: 2019-08-24 | Delivery Date: 2019-08-28',id:6 },
+            { orderTitle: 'Anwar Corporation', orderDetail:'Order #: PK0000517 | Order Date: 2019-08-24 | Delivery Date: 2019-08-28',id:7 }
+          ]
+      })
+
     useEffect( ()=>{
     
         if(Platform.OS==='android')
@@ -16,9 +30,53 @@ export default function MyOrders(props) {
 
         header:{
             backgroundColor:'#6DB33F',
+        },
+        card :{
+          elevation: 8
+        },
+        cardView:{
+          padding:15,
+          backgroundColor:"#DFEED7"
+        },
+        myButton :{
+          padding: 5,
+          height: 50,
+          width: 50,  //The Width must be the same as the height
+          borderRadius:100, //Then Make the Border Radius twice the size of width or Height   
+          backgroundColor:'#60993A',
+          alignItems:'center',
+          justifyContent:'center'
+       
         }
       })
 
+      const searchBar = state.searchBar==true? (
+      (
+                <Item>
+                  <Input placeholder="Search" />
+                  <Icon type="MaterialCommunityIcons" name="shopping" /> 
+              </Item>
+      )
+      ):null
+
+    const orders = state.orders.map(order => {
+    return (
+                 <Card style={styles.card} key={order.id}>
+                    <CardItem>
+                      <Left>
+                        <View style={styles.myButton}>
+                          <Icon type="MaterialCommunityIcons" name="shopping" style={{color:'#ffffff'}}/>
+                        </View>
+                        <Body>
+                          <H2 style={{color:'#6DB33F'}}>{order.orderTitle}</H2>
+                          <Text style={{fontSize:10,color:'#838383'}}>{order.orderDetail}</Text>
+                        </Body>
+                      </Left>
+                    </CardItem>
+                  </Card>
+      )
+    })      
+      
     return (
      <Container style={{backgroundColor:"#DFEED7"}}>
         <Header style={styles.header}>
@@ -28,10 +86,10 @@ export default function MyOrders(props) {
             </Button>
           </Left>
           <Body>
-            <Title><H1 style={{color:'#ffffff'}}>Orders</H1></Title>
+            <Title><H2 style={{color:'#ffffff'}}>Orders</H2></Title>
           </Body>
           <Right>
-            <Button transparent>
+            <Button onPress={()=>setState({searchBar:!state.searchBar})} transparent>
               <Icon style={{color:'#ffffff'}} name='search' />
             </Button>
             <Button onPress={()=>props.navigation.navigate('NewOrders') } transparent>
@@ -41,142 +99,20 @@ export default function MyOrders(props) {
           </Right>
         </Header>
         <Content>
+        
+              {searchBar}
               <Tabs tabBarUnderlineStyle={{backgroundColor:'#ffffff'}}>
                 <Tab  heading="Delivered" activeTextStyle={{color:"#ffffff"}} activeTabStyle={{backgroundColor:'#60993A'}} tabStyle={{backgroundColor:'#60993A'}} textStyle={{color:'#ffffff'}}>
-                <View style={{padding:15,backgroundColor:"#DFEED7"}}>
-                        <Card >
-                          <CardItem>
-                            <Body>
-                              <Text>
-                              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum at dolores ea quas fugiat incidunt ab optio sequi, et, dolorem, laborum vero! Nisi dolores modi aliquid delectus earum. Molestias, cumque?
-                              </Text>
-                            </Body>
-                          </CardItem>
-                        </Card>
-                        <Card>
-                          <CardItem>
-                            <Body>
-                              <Text>
-                              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum at dolores ea quas fugiat incidunt ab optio sequi, et, dolorem, laborum vero! Nisi dolores modi aliquid delectus earum. Molestias, cumque?
-                              </Text>
-                            </Body>
-                          </CardItem>
-                        </Card>
-                        <Card>
-                          <CardItem>
-                            <Body>
-                              <Text>
-                              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum at dolores ea quas fugiat incidunt ab optio sequi, et, dolorem, laborum vero! Nisi dolores modi aliquid delectus earum. Molestias, cumque?
-                              </Text>
-                            </Body>
-                          </CardItem>
-                        </Card>
-                        <Card>
-                          <CardItem>
-                            <Body>
-                              <Text>
-                              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum at dolores ea quas fugiat incidunt ab optio sequi, et, dolorem, laborum vero! Nisi dolores modi aliquid delectus earum. Molestias, cumque?
-                              </Text>
-                            </Body>
-                          </CardItem>
-                        </Card>
-                        <Card>
-                          <CardItem>
-                            <Body>
-                              <Text>
-                              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum at dolores ea quas fugiat incidunt ab optio sequi, et, dolorem, laborum vero! Nisi dolores modi aliquid delectus earum. Molestias, cumque?
-                              </Text>
-                            </Body>
-                          </CardItem>
-                        </Card>
-                        <Card>
-                          <CardItem>
-                            <Body>
-                              <Text>
-                              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum at dolores ea quas fugiat incidunt ab optio sequi, et, dolorem, laborum vero! Nisi dolores modi aliquid delectus earum. Molestias, cumque?
-                              </Text>
-                            </Body>
-                          </CardItem>
-                        </Card>
-                        <Card>
-                          <CardItem>
-                            <Body>
-                              <Text>
-                              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum at dolores ea quas fugiat incidunt ab optio sequi, et, dolorem, laborum vero! Nisi dolores modi aliquid delectus earum. Molestias, cumque?
-                              </Text>
-                            </Body>
-                          </CardItem>
-                        </Card>
+                        <View style={styles.cardView}>
+                          {orders}
                         </View>
                 </Tab>
 
                 <Tab heading="Pending" activeTextStyle={{color:"#ffffff"}} activeTabStyle={{backgroundColor:'#B35644'}}  tabStyle={{backgroundColor:'#B35644'}}  textStyle={{color:'#ffffff'}}>
 
-                <View style={{padding:15,backgroundColor:"#DFEED7"}}>
-                        <Card >
-                          <CardItem>
-                            <Body>
-                              <Text>
-                              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum at dolores ea quas fugiat incidunt ab optio sequi, et, dolorem, laborum vero! Nisi dolores modi aliquid delectus earum. Molestias, cumque?
-                              </Text>
-                            </Body>
-                          </CardItem>
-                        </Card>
-                        <Card>
-                          <CardItem>
-                            <Body>
-                              <Text>
-                              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum at dolores ea quas fugiat incidunt ab optio sequi, et, dolorem, laborum vero! Nisi dolores modi aliquid delectus earum. Molestias, cumque?
-                              </Text>
-                            </Body>
-                          </CardItem>
-                        </Card>
-                        <Card>
-                          <CardItem>
-                            <Body>
-                              <Text>
-                              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum at dolores ea quas fugiat incidunt ab optio sequi, et, dolorem, laborum vero! Nisi dolores modi aliquid delectus earum. Molestias, cumque?
-                              </Text>
-                            </Body>
-                          </CardItem>
-                        </Card>
-                        <Card>
-                          <CardItem>
-                            <Body>
-                              <Text>
-                              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum at dolores ea quas fugiat incidunt ab optio sequi, et, dolorem, laborum vero! Nisi dolores modi aliquid delectus earum. Molestias, cumque?
-                              </Text>
-                            </Body>
-                          </CardItem>
-                        </Card>
-                        <Card>
-                          <CardItem>
-                            <Body>
-                              <Text>
-                              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum at dolores ea quas fugiat incidunt ab optio sequi, et, dolorem, laborum vero! Nisi dolores modi aliquid delectus earum. Molestias, cumque?
-                              </Text>
-                            </Body>
-                          </CardItem>
-                        </Card>
-                        <Card>
-                          <CardItem>
-                            <Body>
-                              <Text>
-                              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum at dolores ea quas fugiat incidunt ab optio sequi, et, dolorem, laborum vero! Nisi dolores modi aliquid delectus earum. Molestias, cumque?
-                              </Text>
-                            </Body>
-                          </CardItem>
-                        </Card>
-                        <Card>
-                          <CardItem>
-                            <Body>
-                              <Text>
-                              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum at dolores ea quas fugiat incidunt ab optio sequi, et, dolorem, laborum vero! Nisi dolores modi aliquid delectus earum. Molestias, cumque?
-                              </Text>
-                            </Body>
-                          </CardItem>
-                        </Card>
-                        </View>
+                   <View style={styles.cardView}>
+                      {orders}
+                   </View>
                 </Tab>
               </Tabs>
 
@@ -187,3 +123,4 @@ export default function MyOrders(props) {
 
     )
 }
+export default  MyOrders;
